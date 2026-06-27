89fb5dcf50da642408f90d75e1c28fcc6a419efb
magboor0789-lab <magboor.0789@gmail.com>
2026-06-27 22:31:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,18 @@
 from aiogram import Bot, Dispatcher, executor, types
 import os
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+TOKEN = os.getenv("BOT_TOKEN")
 
-bot = Bot(token=BOT_TOKEN)
+bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
 
-
-@dp.message_handler(commands=['start'])
+@dp.message_handler(commands=["start"])
 async def start(message: types.Message):
-    await message.reply("✅ البوت يعمل بنجاح")
+    await message.reply("🚀 البوت يعمل بنجاح")
 
-
-@dp.message_handler(commands=['help'])
+@dp.message_handler(commands=["help"])
 async def help_command(message: types.Message):
-    await message.reply("الأوامر:\n/start\n/help")
-
+    await message.reply("✅ البوت جاهز")
 
 if __name__ == "__main__":
     executor.start_polling(dp, skip_updates=True)
